fa79b7680d8dec5ae57b6b29cd918ddd5754fd86
Hacknel <alexis.00363@outlook.com>
2020-06-03 09:05:21 -0600

    Agredado Ejercicio Manejo Cadenas | Strings | 03/06/2020

---
+++ I Parcial/Ejericios Clase/EjercicioClaseManejoSrings/EjercicioClaseManejoSrings/Form1.Designer.vb
@@ -0,0 +1,249 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ManejoStrings
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtCadena = New System.Windows.Forms.TextBox()
+        Me.txtLonguitud = New System.Windows.Forms.TextBox()
+        Me.txtUltimoCaracter = New System.Windows.Forms.TextBox()
+        Me.txtMinusculas = New System.Windows.Forms.TextBox()
+        Me.txtBorrarEspacios = New System.Windows.Forms.TextBox()
+        Me.txtMayusculas = New System.Windows.Forms.TextBox()
+        Me.txtPosB = New System.Windows.Forms.TextBox()
+        Me.txt3ro8vo = New System.Windows.Forms.TextBox()
+        Me.txtPrimerCaracter = New System.Windows.Forms.TextBox()
+        Me.btnMostrar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(62, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Cadena"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(62, 125)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Ultimo Caracter"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(62, 85)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(54, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Longuitud"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(62, 201)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(54, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "3ro al 8vo"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(62, 241)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(106, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Posicion de la letra B"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(62, 164)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(79, 13)
+        Me.Label6.TabIndex = 3
+        Me.Label6.Text = "Primer Caracter"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(62, 314)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(60, 13)
+        Me.Label7.TabIndex = 8
+        Me.Label7.Text = "Minusculas"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(62, 354)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(81, 13)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Borrar Espacios"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(62, 277)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(63, 13)
+        Me.Label9.TabIndex = 6
+        Me.Label9.Text = "Mayusculas"
+        '
+        'txtCadena
+        '
+        Me.txtCadena.Location = New System.Drawing.Point(174, 48)
+        Me.txtCadena.Name = "txtCadena"
+        Me.txtCadena.Size = New System.Drawing.Size(100, 20)
+        Me.txtCadena.TabIndex = 9
+        '
+        'txtLonguitud
+        '
+        Me.txtLonguitud.Location = New System.Drawing.Point(174, 85)
+        Me.txtLonguitud.Name = "txtLonguitud"
+        Me.txtLonguitud.Size = New System.Drawing.Size(100, 20)
+        Me.txtLonguitud.TabIndex = 10
+        '
+        'txtUltimoCaracter
+        '
+        Me.txtUltimoCaracter.Location = New System.Drawing.Point(174, 125)
+        Me.txtUltimoCaracter.Name = "txtUltimoCaracter"
+        Me.txtUltimoCaracter.Size = New System.Drawing.Size(100, 20)
+        Me.txtUltimoCaracter.TabIndex = 11
+        '
+        'txtMinusculas
+        '
+        Me.txtMinusculas.Location = New System.Drawing.Point(174, 314)
+        Me.txtMinusculas.Name = "txtMinusculas"
+        Me.txtMinusculas.Size = New System.Drawing.Size(100, 20)
+        Me.txtMinusculas.TabIndex = 13
+        '
+        'txtBorrarEspacios
+        '
+        Me.txtBorrarEspacios.Location = New System.Drawing.Point(174, 354)
+        Me.txtBorrarEspacios.Name = "txtBorrarEspacios"
+        Me.txtBorrarEspacios.Size = New System.Drawing.Size(100, 20)
+        Me.txtBorrarEspacios.TabIndex = 14
+        '
+        'txtMayusculas
+        '
+        Me.txtMayusculas.Location = New System.Drawing.Point(174, 277)
+        Me.txtMayusculas.Name = "txtMayusculas"
+        Me.txtMayusculas.Size = New System.Drawing.Size(100, 20)
+        Me.txtMayusculas.TabIndex = 15
+        '
+        'txtPosB
+        '
+        Me.txtPosB.Location = New System.Drawing.Point(174, 238)
+        Me.txtPosB.Name = "txtPosB"
+        Me.txtPosB.Size = New System.Drawing.Size(100, 20)
+        Me.txtPosB.TabIndex = 16
+        '
+        'txt3ro8vo
+        '
+        Me.txt3ro8vo.Location = New System.Drawing.Point(174, 201)
+        Me.txt3ro8vo.Name = "txt3ro8vo"
+        Me.txt3ro8vo.Size = New System.Drawing.Size(100, 20)
+        Me.txt3ro8vo.TabIndex = 17
+        '
+        'txtPrimerCaracter
+        '
+        Me.txtPrimerCaracter.Location = New System.Drawing.Point(174, 161)
+        Me.txtPrimerCaracter.Name = "txtPrimerCaracter"
+        Me.txtPrimerCaracter.Size = New System.Drawing.Size(100, 20)
+        Me.txtPrimerCaracter.TabIndex = 18
+        '
+        'btnMostrar
+        '
+        Me.btnMostrar.Location = New System.Drawing.Point(337, 201)
+        Me.btnMostrar.Name = "btnMostrar"
+        Me.btnMostrar.Size = New System.Drawing.Size(75, 23)
+        Me.btnMostrar.TabIndex = 19
+        Me.btnMostrar.Text = "Mostrar"
+        Me.btnMostrar.UseVisualStyleBackColor = True
+        '
+        'ManejoStrings
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(477, 450)
+        Me.Controls.Add(Me.btnMostrar)
+        Me.Controls.Add(Me.txtPrimerCaracter)
+        Me.Controls.Add(Me.txt3ro8vo)
+        Me.Controls.Add(Me.txtPosB)
+        Me.Controls.Add(Me.txtMayusculas)
+        Me.Controls.Add(Me.txtBorrarEspacios)
+        Me.Controls.Add(Me.txtMinusculas)
+        Me.Controls.Add(Me.txtUltimoCaracter)
+        Me.Controls.Add(Me.txtLonguitud)
+        Me.Controls.Add(Me.txtCadena)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "ManejoStrings"
+        Me.Text = "Manejo Strings"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents txtCadena As TextBox
+    Friend WithEvents txtLonguitud As TextBox
+    Friend WithEvents txtUltimoCaracter As TextBox
+    Friend WithEvents txtMinusculas As TextBox
+    Friend WithEvents txtBorrarEspacios As TextBox
+    Friend WithEvents txtMayusculas As TextBox
+    Friend WithEvents txtPosB As TextBox
+    Friend WithEvents txt3ro8vo As TextBox
+    Friend WithEvents txtPrimerCaracter As TextBox
+    Friend WithEvents btnMostrar As Button
+End Class
---
+++ I Parcial/Ejericios Clase/EjercicioClaseManejoSrings/EjercicioClaseManejoSrings/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class ManejoStrings
+    Private Sub btnMostrar_Click(sender As Object, e As EventArgs) Handles btnMostrar.Click
+        Try
+            Dim cadena As String
+            If IsNumeric(txtCadena.Text) = False And txtCadena.Text <> String.Empty Then
+                cadena = txtCadena.Text
+                txtLonguitud.Text = cadena.Length
+                txtUltimoCaracter.Text = cadena.Substring(cadena.Length - 1, 1)
+                'substring(posicionCaracter, Cantidad de Caracteres)
+                txtPrimerCaracter.Text = cadena.Substring(0, 1)
+                txt3ro8vo.Text = cadena.Substring(2, 7)
+                txtPosB.Text = cadena.IndexOf("B", 0) + 1
+                txtMayusculas.Text = cadena.ToUpper
+                txtMinusculas.Text = cadena.ToLower
+                txtBorrarEspacios.Text = cadena.Replace("", "")
+            Else
+                MessageBox.Show("Ingrese Valores correctos!")
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+
+
+    End Sub
+End Class
